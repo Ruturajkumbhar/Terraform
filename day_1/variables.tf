84ade91e1d48801c34c91b43ff2938dd195e7940
ruturaj <ruturajkumbhar2612@gmail.com>
2026-01-03 18:17:02 +0530

    day1

---
+++ day_1/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
+
+variable "type" {
+    type = string
+    default = ""
+  
+}
+
+variable "name" {
+  type = string
+  default = "first_server"
+}
